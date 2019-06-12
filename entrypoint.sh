753a0b7e01bc09a7f3c284cf313720aafb1216ee
Dom Sekotill <dom.sekotill@kodo.org.uk>
2019-06-13 00:06:04 +0100

    Add a licence (MPL-2.0)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#
+# Copyright (c) 2019 Dominik Sekotill <dom.sekotill@kodo.org.uk>
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
 set -eu -o pipefail
 shopt -s nullglob globstar
 
