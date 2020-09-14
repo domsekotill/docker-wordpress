d275faa95b4495d549cbbc68220507d45046637d
Dom Sekotill <dom.sekotill@kodo.org.uk>
2020-09-14 19:07:10 +0100

    Block all plugin & theme changes from admin pages

--- plugins/override_plugin_install.php
+++ plugins/override_plugin_install.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name: Disable New Plugins
+ * Plugin Name: Disable Plugins & Theme Management
  * Plugin URI: https://code.kodo.org.uk/singing-chimes.co.uk/wordpress/tree/master/plugins
- * Description: Removes the "Add Plugin" button from the admin dashboard
+ * Description: Prevents changes to installed code from the admin dashboard.  All such changes should be managed through deployment configuration.
  * Licence: MPL-2.0
  * Licence URI: https://www.mozilla.org/en-US/MPL/2.0/
  * Author: Dominik Sekotill
@@ -12,7 +12,15 @@
 add_filter( 'user_has_cap', 'override_plugin_install', 10, 3 );
 
 function override_plugin_install( $allcaps, $caps, $args ) {
-	if ( 'install_plugins' == $args[0] ) {
+	switch ($args[0]) {
+	case 'delete_plugins':
+	case 'delete_themes':
+	case 'edit_plugins':
+	case 'edit_themes':
+	case 'install_plugins':
+	case 'install_themes':
+	case 'update_plugins':
+	case 'update_themes':
 		$allcaps[$caps[0]] = false;
 	}
 	return $allcaps;
