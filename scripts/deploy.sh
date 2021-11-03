fadbf5c614867d16ee500c76fad96d6a7a1ead23
Dom Sekotill <dom.sekotill@kodo.org.uk>
2021-11-03 23:16:11 +0000

    Upgrade to 'rules' and 'extends' keywords in CI
    No CI behaviour changes should occur as a result of this change, this
    is just an upgrade that should improve maintainability.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
+# Copyright 2019-2021 Dominik Sekotill <dom.sekotill@kodo.org.uk>
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 set -eu
 
 : ${BUILD_REPO?}
 : ${DEPLOY_REPO?}
 
-case ${CI_COMMIT_REF_NAME-develop} in
-	master) tags="latest ${VERSION-}" ;;
-	develop) tags="unstable" ;;
-	*) exit 3 ;;
-esac
-
 set -x
 
 docker pull ${BUILD_REPO}
 
-for tag in $tags; do
+for tag in "$@"; do
 	docker tag ${BUILD_REPO} ${DEPLOY_REPO}:${tag}
 	docker push ${DEPLOY_REPO}:${tag}
 done
