0cb615e85720745338f578b18c329b468335ccc4
Dom Sekotill <dom.sekotill@kodo.org.uk>
2022-10-21 17:35:40 +0100

    Bump copyright dates for files merged in 2022

--- tests/environment.py
+++ tests/environment.py
@@ -1,4 +1,4 @@
-#  Copyright 2021  Dominik Sekotill <dom.sekotill@kodo.org.uk>
+#  Copyright 2021-2022  Dominik Sekotill <dom.sekotill@kodo.org.uk>
 #
 #  This Source Code Form is subject to the terms of the Mozilla Public
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
