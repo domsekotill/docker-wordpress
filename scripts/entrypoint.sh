domsekotill/docker-wordpress

commit 5c1e7c08b363567945c59eee4d50aa1b5988169b

Fix issue where sensitive envvars not removed if DB already initialised

scripts/entrypoint.sh
@@ -82,6 +82,13 @@ create_config()
 }
 
 setup_database() {
+	local admin_name=${SITE_ADMIN:-admin}
+	local admin_email=${SITE_ADMIN_EMAIL:-admin@$domain}
+	local admin_password=${SITE_ADMIN_PASSWORD-}
+
+	# Clear potentialy sensitive information from environment lest it leaks
+	unset ${!SITE_ADMIN*}
+
 	wp core is-installed && return
 
 	local domain=${SITE_URL#*://}
@@ -90,12 +97,9 @@ setup_database() {
 	wp core install \
 		--url="${SITE_URL%/}" \
 		--title="${SITE_TITLE:-New Wordpress Site}" \
-		--admin_user="${SITE_ADMIN:-admin}" \
-		--admin_email="${SITE_ADMIN_EMAIL:-admin@$domain}" \
-		${SITE_ADMIN_PASSWORD+--admin_password="${SITE_ADMIN_PASSWORD}"}
-
-	# Clear potentialy sensitive information from environment lest it leaks
-	unset ${!SITE_ADMIN*}
+		--admin_user="${admin_name}" \
+		--admin_email="${admin_email}" \
+		${admin_password:+--admin_password="${admin_password}"}
 
 	# Start with a pretty, restful permalink structure, instead of the plain,
 	# ugly default. The user can change this as they please through the admin

commit 544736835b314c36ea8150e7a8c3dd737c19cd1e

Fix bug introduced by 5c1e7c08

scripts/entrypoint.sh
@@ -82,6 +82,9 @@ create_config()
 }
 
 setup_database() {
+	local domain=${SITE_URL#*://}
+	domain=${domain%%[:/]*}
+
 	local admin_name=${SITE_ADMIN:-admin}
 	local admin_email=${SITE_ADMIN_EMAIL:-admin@$domain}
 	local admin_password=${SITE_ADMIN_PASSWORD-}
@@ -90,9 +93,6 @@ setup_database() {
 	unset ${!SITE_ADMIN*}
 
 	wp core is-installed && return
-
-	local domain=${SITE_URL#*://}
-	domain=${domain%%[:/]*}
 
 	wp core install \
 		--url="${SITE_URL%/}" \